b4d6966920fb3e256f3d497eb8bc09069f773654
humboldt <humboldt_xie@163.com>
2013-11-13 11:16:55 +0800

    add shdict string time

---
+++ src/ngx_tcp_lua_string.h
@@ -0,0 +1,12 @@
+#ifndef NGX_TCP_LUA_STRING_H
+#define NGX_TCP_LUA_STRING_H
+
+
+#include "ngx_tcp_lua_common.h"
+
+
+void ngx_tcp_lua_inject_string_api(lua_State *L);
+
+
+#endif /* NGX_TCP_LUA_STRING_H */
+
